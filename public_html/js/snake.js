5ea443c58248543acea6316994ef13eb1c118592
Jackson Mcglasson <originalqwerty45@gmaill.com>
2014-10-11 20:32:47 -0700

    added snake direction variable

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -6,6 +6,7 @@
 var snake;
 var snakeLength;
 var snakeSize;
+var snakeDirection;
 
 var food;
 
@@ -59,6 +60,7 @@ function snakeInitialize(){
     snake = [];
     snakeLength = 5;
     snakeSize = 18;
+    snakeDirection = "right";
     
     for(var index = snakeLength - 1; index >= 0; index--) {
         snake.push( {
@@ -79,122 +81,14 @@ function snakeUpdate(){
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
     
-    snakeHeadX++;
+    if(snakeDirection == "right") {
+        snakeHeadX++;
+    }
     
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
     snakeTail.y = snakeHeadY;
     snake.unshift(snakeTail);
-}
-
-document.onkeydown = function(event) {
-  var keyCode; 
-
-  if(event == null)
-  {
-    keyCode = window.event.keyCode; 
-  }
-  else 
-  {
-    keyCode = event.keyCode; 
-  }
-
-  switch(keyCode)
-  {
-    // left 
-    case 37:
-      moveLeft();
-      break;
-
-    // up 
-    case 38:
-      moveUp();
-      break; 
-
-    // right 
-    case 39:
-      moveRight();
-      break; 
-
-    // down
-    case 40:
-      moveDown();
-      break; 
-
-    default: 
-      break; 
-  } 
-};
-
-function moveSnake(){
-  switch(direction){
-    case 'up':
-      moveUp();
-      break;
-
-    case 'down':
-      moveDown();
-      break;
-
-    case 'left':
-      moveLeft();
-      break;
-
-    case 'right':
-      moveRight();
-      break;
-  }
-}
-
-function whichWayToGo(axisType){  
-  if (axisType=='x') {
-    a = (currentPosition['x'] > canvas.width / 2) ? moveLeft() : moveRight();
-  } else {
-    a = (currentPosition['y'] > canvas.height / 2) ? moveUp() : moveDown();
-  }
-}
-
-function moveUp(){
-  if (upPosition() >= 0) {
-    executeMove('up', 'y', upPosition());
-  } else {
-    whichWayToGo('x');
-  }
-}
-
-function moveDown(){
-  if (downPosition() < canvas.height) {
-    executeMove('down', 'y', downPosition());    
-  } else {
-    whichWayToGo('x');
-  }
-}
-
-function moveLeft(){
-  if (leftPosition() >= 0) {
-    executeMove('left', 'x', leftPosition());
-  } else {
-    whichWayToGo('y');
-  }
-}
-
-function moveRight(){
-  if (rightPosition() < canvas.width) {
-    executeMove('right', 'x', rightPosition());
-  } else {
-    whichWayToGo('y');
-  }
-}
-
-snakeBody = [];
-
-function drawSnake() {
-  snakeBody.push([currentPosition['x'], currentPosition['y']]);
-  ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
-  if (snakeBody.length > 3) {
-    var itemToRemove = snakeBody.shift();
-    ctx.clearRect(itemToRemove[0], itemToRemove[1], gridSize, gridSize);
-  }
 }
 
 /*-----------------------------------------------------------------------------
